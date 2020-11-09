454e680b7793342fa14ee0cdb51fbeb8621876b8
Dametres <dametres.perry21@gmail.com>
2020-11-09 14:49:53 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def initialize(name, artist, genre)
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+        @name = name
+        @artist = artist
+        @genre = genre
+    end
+
+    def self.count 
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        genre_hash = Hash.new(0)
+       @@genres.each { |genre| genre_hash[genre] += 1 }
+         genre_hash
+       end 
+       
+       def self.artist_count
+          artists_hash = Hash.new(0)
+          @@artists.each { |artist| artists_hash[artist] += 1 }
+          artists_hash
+       end 
+end
